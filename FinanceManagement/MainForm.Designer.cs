412d88255323b69492dc7643e8555dd857462fe4
KwonSC <comingo1@naver.com>
2018-08-08 10:14:23 +0900

    데이터그리드뷰 삭제

--- FinanceManagement/MainForm.Designer.cs
+++ FinanceManagement/MainForm.Designer.cs
@@ -25,7 +25,11 @@
         private void InitializeComponent() {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(재정관리));
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.테이블1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database1DataSet1 = new FinanceManagement.Database1DataSet1();
+            this.database1DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database1DataSet = new FinanceManagement.Database1DataSet();
+            this.테이블1TableAdapter = new FinanceManagement.Database1DataSet1TableAdapters.테이블1TableAdapter();
             this.파일 = new System.Windows.Forms.ToolStripMenuItem();
             this.fileCreate = new System.Windows.Forms.ToolStripMenuItem();
             this.fileOpen = new System.Windows.Forms.ToolStripMenuItem();
@@ -36,35 +40,37 @@
             this.환경설정 = new System.Windows.Forms.ToolStripMenuItem();
             this.budgetSetting = new System.Windows.Forms.ToolStripMenuItem();
             this.carryoverSetting = new System.Windows.Forms.ToolStripMenuItem();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.테이블1BindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.database1DataSet1 = new FinanceManagement.Database1DataSet1();
-            this.database1DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.database1DataSet = new FinanceManagement.Database1DataSet();
-            this.테이블1TableAdapter = new FinanceManagement.Database1DataSet1TableAdapters.테이블1TableAdapter();
-            this.menuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             ((System.ComponentModel.ISupportInitialize)(this.테이블1BindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.database1DataSet1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.database1DataSetBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // menuStrip1
+            // 테이블1BindingSource
             // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.파일,
-            this.등록,
-            this.검색,
-            this.환경설정});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(700, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
+            this.테이블1BindingSource.DataMember = "테이블1";
+            this.테이블1BindingSource.DataSource = this.database1DataSet1;
+            // 
+            // database1DataSet1
+            // 
+            this.database1DataSet1.DataSetName = "Database1DataSet1";
+            this.database1DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // database1DataSetBindingSource
+            // 
+            this.database1DataSetBindingSource.DataSource = this.database1DataSet;
+            this.database1DataSetBindingSource.Position = 0;
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // 테이블1TableAdapter
+            // 
+            this.테이블1TableAdapter.ClearBeforeFill = true;
             // 
             // 파일
             // 
@@ -141,57 +147,26 @@
             this.carryoverSetting.Text = "이월금 변경";
             this.carryoverSetting.Click += new System.EventHandler(this.carryoverSetting_Click);
             // 
-            // dataGridView1
+            // menuStrip1
             // 
-            this.dataGridView1.AutoGenerateColumns = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.iDDataGridViewTextBoxColumn});
-            this.dataGridView1.DataSource = this.테이블1BindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 26);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 27;
-            this.dataGridView1.Size = new System.Drawing.Size(408, 204);
-            this.dataGridView1.TabIndex = 1;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            // 
-            // iDDataGridViewTextBoxColumn
-            // 
-            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
-            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
-            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
-            // 
-            // 테이블1BindingSource
-            // 
-            this.테이블1BindingSource.DataMember = "테이블1";
-            this.테이블1BindingSource.DataSource = this.database1DataSet1;
-            // 
-            // database1DataSet1
-            // 
-            this.database1DataSet1.DataSetName = "Database1DataSet1";
-            this.database1DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // database1DataSetBindingSource
-            // 
-            this.database1DataSetBindingSource.DataSource = this.database1DataSet;
-            this.database1DataSetBindingSource.Position = 0;
-            // 
-            // database1DataSet
-            // 
-            this.database1DataSet.DataSetName = "Database1DataSet";
-            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // 테이블1TableAdapter
-            // 
-            this.테이블1TableAdapter.ClearBeforeFill = true;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일,
+            this.등록,
+            this.검색,
+            this.환경설정});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(786, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
             // 
             // 재정관리
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(700, 360);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(786, 483);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
@@ -200,21 +175,25 @@
             this.Text = "Zacchaeus";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.재정관리_Load);
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.테이블1BindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.database1DataSet1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.database1DataSetBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.BindingSource database1DataSetBindingSource;
+        private Database1DataSet database1DataSet;
+        private Database1DataSet1 database1DataSet1;
+        private System.Windows.Forms.BindingSource 테이블1BindingSource;
+        private Database1DataSet1TableAdapters.테이블1TableAdapter 테이블1TableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn field1DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn field2DataGridViewTextBoxColumn;
         private System.Windows.Forms.ToolStripMenuItem 파일;
         private System.Windows.Forms.ToolStripMenuItem fileCreate;
         private System.Windows.Forms.ToolStripMenuItem fileOpen;
@@ -225,15 +204,7 @@
         private System.Windows.Forms.ToolStripMenuItem 환경설정;
         private System.Windows.Forms.ToolStripMenuItem budgetSetting;
         private System.Windows.Forms.ToolStripMenuItem carryoverSetting;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.BindingSource database1DataSetBindingSource;
-        private Database1DataSet database1DataSet;
-        private Database1DataSet1 database1DataSet1;
-        private System.Windows.Forms.BindingSource 테이블1BindingSource;
-        private Database1DataSet1TableAdapters.테이블1TableAdapter 테이블1TableAdapter;
-        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn field1DataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn field2DataGridViewTextBoxColumn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
     }
 }
 
