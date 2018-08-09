985f26763011e306529cd1cb0ccf82ba0f216d8b
KwonSC <comingo1@naver.com>
2018-08-09 10:35:37 +0900

    UI  재배치

--- FinanceManagement/Register.Designer.cs
+++ FinanceManagement/Register.Designer.cs
@@ -24,91 +24,288 @@
         /// </summary>
         private void InitializeComponent() {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Register));
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.Register_choice = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.imcomePanel = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.Name1 = new System.Windows.Forms.TextBox();
+            this.Name2 = new System.Windows.Forms.TextBox();
+            this.Name3 = new System.Windows.Forms.TextBox();
+            this.Sum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Note = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel1.Controls.Add(this.Register_choice);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(5, 2);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(104, 442);
-            this.panel1.TabIndex = 0;
-            // 
-            // Register_choice
-            // 
-            this.Register_choice.AutoSize = true;
-            this.Register_choice.BackColor = System.Drawing.SystemColors.Info;
-            this.Register_choice.Font = new System.Drawing.Font("굴림", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.Register_choice.ForeColor = System.Drawing.Color.Black;
-            this.Register_choice.Location = new System.Drawing.Point(29, 6);
-            this.Register_choice.Name = "Register_choice";
-            this.Register_choice.Size = new System.Drawing.Size(47, 19);
-            this.Register_choice.TabIndex = 2;
-            this.Register_choice.Text = "선택";
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("굴림", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.button2.Location = new System.Drawing.Point(5, 120);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(95, 74);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "지출부";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("굴림", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.button1.Location = new System.Drawing.Point(5, 41);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(12, 16);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(95, 74);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "수입부";
+            this.button1.Size = new System.Drawing.Size(69, 62);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "수입";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // imcomePanel
+            // button2
             // 
-            this.imcomePanel.Location = new System.Drawing.Point(115, 37);
-            this.imcomePanel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.imcomePanel.Name = "imcomePanel";
-            this.imcomePanel.Size = new System.Drawing.Size(743, 279);
-            this.imcomePanel.TabIndex = 1;
+            this.button2.Location = new System.Drawing.Point(12, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 64);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "지출";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(154, 41);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(110, 21);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(154, 78);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(108, 16);
+            this.listBox1.TabIndex = 4;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(154, 112);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(108, 16);
+            this.listBox2.TabIndex = 5;
+            // 
+            // Name1
+            // 
+            this.Name1.Location = new System.Drawing.Point(154, 140);
+            this.Name1.Name = "Name1";
+            this.Name1.Size = new System.Drawing.Size(120, 21);
+            this.Name1.TabIndex = 6;
+            // 
+            // Name2
+            // 
+            this.Name2.Location = new System.Drawing.Point(154, 180);
+            this.Name2.Name = "Name2";
+            this.Name2.Size = new System.Drawing.Size(120, 21);
+            this.Name2.TabIndex = 7;
+            // 
+            // Name3
+            // 
+            this.Name3.Location = new System.Drawing.Point(154, 219);
+            this.Name3.Name = "Name3";
+            this.Name3.Size = new System.Drawing.Size(120, 21);
+            this.Name3.TabIndex = 8;
+            // 
+            // Sum
+            // 
+            this.Sum.Location = new System.Drawing.Point(362, 140);
+            this.Sum.Name = "Sum";
+            this.Sum.Size = new System.Drawing.Size(120, 21);
+            this.Sum.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "이름1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "이름2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(104, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "이름3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(321, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "금액";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(321, 189);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "비고";
+            // 
+            // Note
+            // 
+            this.Note.Location = new System.Drawing.Point(362, 180);
+            this.Note.Name = "Note";
+            this.Note.Size = new System.Drawing.Size(478, 21);
+            this.Note.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(104, 116);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 12);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "목";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(104, 47);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 12);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "날짜";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(104, 82);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 12);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "항";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(106, 256);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(55, 46);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "저장";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(167, 256);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(55, 46);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "수정";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(228, 256);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(55, 46);
+            this.button5.TabIndex = 21;
+            this.button5.Text = "삭제";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(289, 256);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(55, 46);
+            this.button6.TabIndex = 22;
+            this.button6.Text = "정렬";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(108, 311);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(731, 104);
+            this.dataGridView1.TabIndex = 23;
             // 
             // Register
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(859, 442);
-            this.Controls.Add(this.imcomePanel);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(859, 482);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Note);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Sum);
+            this.Controls.Add(this.Name3);
+            this.Controls.Add(this.Name2);
+            this.Controls.Add(this.Name1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Register";
             this.Text = "수입 지출 등록";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label Register_choice;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Panel imcomePanel;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox Name1;
+        private System.Windows.Forms.TextBox Name2;
+        private System.Windows.Forms.TextBox Name3;
+        private System.Windows.Forms.TextBox Sum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Note;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
