da8bf3ec120dd5a14246e6d1103838ad7ac3bcc7
KwonSC <comingo1@naver.com>
2018-08-13 13:49:13 +0900

    ㅇㄴㅁㅇㅁㄴ

--- FinanceManagement/Register.Designer.cs
+++ FinanceManagement/Register.Designer.cs
@@ -105,7 +105,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(787, 470);
             this.panel1.TabIndex = 3;
-            this.panel1.Visible = false;
             // 
             // label10
             // 
